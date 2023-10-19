7aab64f5164a4dc67d0dd7f6e53ec743ad560a0c
irixalmeida <irinhaalmeida222@gmail.com>
2023-10-19 11:33:09 -0300

    adiciona a Cli ao projeto e teste da mesma

--- cli.js
+++ cli.js
@@ -1,8 +1,30 @@
-const { soma, readFile } = require("./index.js");
-const chalk = require("chalk");
+#!/usr/bin/env node
+//const { soma } = require("./index");
 
-const resultado = soma(1, 2);
+const args = process.argv.slice(2);
 
-console.log(chalk.yellow(resultado));
+if (args.length === 0) {
+  console.log("Por favor, forneça um argumento.");
+  process.exit(1);
+}
 
-readFile("./test/files/oneFile.md");
+switch (args[0]) {
+  case "saudacao":
+    console.log(`Olá, ${args[1]}!`);
+    break;
+  case "info":
+    console.log("Este é um programa CLI básico.");
+    break;
+  default:
+    console.log("Comando não reconhecido.");
+    break;
+}
+
+//const { soma, readFile } = require("./index.js");
+//const chalk = require("chalk");
+
+//onst resultado = soma(1, 2);
+
+//console.log(chalk.yellow(resultado));
+
+//readFile("./test/files/oneFile.md");
--- index.js
+++ index.js
@@ -13,8 +13,8 @@
 
 //module.exports = { soma, readFile };
 
-function soma(a, b) {
-  return a + b;
-}
+//function soma(a, b) {
+//return a + b;
+//}
 
-module.exports = soma;
+//module.exports = { soma };
